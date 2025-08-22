2b2df68014145e086b213183a3274ea7e0603bd5
Edrick Manoel <edrick_42@icloud.com>
2025-08-21 22:15:05 -0300

    Changing from psqllite to postgres and using json to load the projects

--- thelightofdiya/portfolio/views.py
+++ thelightofdiya/portfolio/views.py
@@ -1,5 +1,13 @@
+import os
+import json
+from django.conf import settings
 from django.shortcuts import render
 
 # Create your views here.
 def portfolio(request):
-    return render(request, 'portfolio.html')
+    file_path = os.path.join(os.path.dirname(__file__), 'data', 'projects.json')
+
+    with open(file_path, 'r', encoding="utf-8") as file:
+        projects = json.load(file)
+
+    return render(request, 'portfolio.html', {'projects': projects})
